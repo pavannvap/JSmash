d6828fe61ee57183deaf81798e6c7b1dea3dd446
Nisum <nisum@Admins-MacBook-Pro.local>
2017-09-02 22:14:13 -0700

    aded method

--- plugin.js
+++ plugin.js
@@ -10,12 +10,9 @@ function multiply(){
     alert('multiply method');
   }
 
-<<<<<<< HEAD
 function unknown(){
     alert('unknown method');
 }
-=======
 function devide(){
     alert('devide method');
   }
->>>>>>> b36cdb875736016345f4d063017221ec061f9075
